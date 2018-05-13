822a4833079edb823386bd42dd9d456edd93cc37
LeUyen Pham <leuyenp@leuyens-french-macbook-pro.local>
2018-05-13 09:34:59 -0700

    Initial Commit

---
+++ AR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AR
+//
+//  Created by LeUyen Pham on 5/13/18.
+//  Copyright © 2018 Leo Commandblocks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AR
+//
+//  Created by LeUyen Pham on 5/13/18.
+//  Copyright © 2018 Leo Commandblocks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
